0db74fc0cec46fe8c23cc234fc440d4548f386ce
= <benleong0@gmail.com>
2021-01-29 17:49:38 +0000

    merged ProjectCards in app (was just loading projectcard's)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import "./components/ProjectCards/ProjectCards.css";
 import HeadingCard from "./components/HeadingCard/HeadingCard";
-import ProjectCards from "./components/ProjectCards/ProjectCards";
+import ProjectCard from "./components/ProjectCards/ProjectCard";
 
 function App() {
+  const projects = require("./components/ProjectCards/projects.json").items;
   return (
     <div className="App">
       <HeadingCard />
-      <ProjectCards />
+      {projects.map(project => (
+        <ProjectCard key={project.id} {...project} />
+      ))}
     </div>
   );
 }
