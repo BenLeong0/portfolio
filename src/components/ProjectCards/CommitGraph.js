bd946814585380125ef06290664947fb0162b821
= <benleong0@gmail.com>
2021-01-29 17:50:04 +0000

    tidying

--- src/components/ProjectCards/CommitGraph.js
+++ src/components/ProjectCards/CommitGraph.js
@@ -5,18 +5,20 @@ function CommitGraph(props) {
   const stroke = "black";
   const strokeWidth = 2;
 
-  const width = 380;
+  const width = 370;
   const height = 240;
   const diff = width / props.graphPoints.length;
   const maxValue = props.graphPoints[props.graphPoints.length - 1];
   var i = -diff / 2;
 
-  const finalPoint =
-    fill !== "none" ? " " + (width - diff / 2) + "," + height : "";
-  const points =
-    props.graphPoints
-      .map(x => [(i += diff), (height / maxValue) * (maxValue - x)])
-      .join(" ") + finalPoint;
+  const points = props.graphPoints.map(x => [
+    (i += diff),
+    (height / maxValue) * (maxValue - x)
+  ]);
+
+  if (fill !== "none") {
+    points.push([width - diff / 2, height]);
+  }
 
   return (
     <svg viewBox={`0 0 ${width} ${height}`}>
--- src/components/ProjectCards/CommitGraphBox.js
+++ src/components/ProjectCards/CommitGraphBox.js
@@ -3,9 +3,9 @@ import * as axios from "axios";
 import CommitGraph from "./CommitGraph";
 
 function CommitGraphBox(props) {
-  // const [state, setState] = useState(0);
   const [endDates, setEndDates] = useState([0, 0]);
   const [graphPoints, setGraphPoints] = useState([0]);
+  const [totalCommits, setTotalCommits] = useState(0);
 
   const convertDay = date => {
     return Math.abs(date) / 86400000 - 18500;
@@ -18,33 +18,30 @@ function CommitGraphBox(props) {
     );
     setEndDates([startDate, endDate]);
 
-    const dates = data.map(
-      commit => new Date(commit.commit.author.date.slice(0, 10))
-    );
     var prevDate = convertDay(startDate) - 1;
-
     var currGraphPoints = [];
     var commits = 0;
 
-    dates
-      .slice()
-      .reverse()
-      .forEach(date => {
-        var newDate = convertDay(date);
-        if (newDate === prevDate) {
-          commits++;
-        } else {
-          currGraphPoints = [
-            ...currGraphPoints,
-            ...Array(newDate - prevDate).fill(commits)
-          ];
-          commits++;
-        }
-        prevDate = newDate;
-      });
+    // .reverse since data is received newest-first
+    // then extract the date and update graph points
+    data.reverse().forEach(commit => {
+      var newDate = convertDay(
+        new Date(commit.commit.author.date.slice(0, 10))
+      );
+      if (newDate === prevDate) {
+        commits++;
+      } else {
+        currGraphPoints = [
+          ...currGraphPoints,
+          ...Array(newDate - prevDate).fill(commits)
+        ];
+        commits++;
+      }
+      prevDate = newDate;
+    });
     currGraphPoints.push(commits);
-    // currGraphPoints[n] = cum. number of commits up to day n after creation
 
+    setTotalCommits(commits);
     setGraphPoints(currGraphPoints);
   };
 
@@ -58,11 +55,15 @@ function CommitGraphBox(props) {
           per_page: 100
         }
       };
+      // Add endDate parameter if present in json
+      if (props.hasOwnProperty("endDate")) {
+        config.params.until = props.endDate;
+      }
+
       const resp = await axios.get(
         `https://api.github.com/repos/${props.github_owner}/${props.github_repo}/commits`,
         config
       );
-      // setState(resp.data);
       updateGraph(resp.data);
     };
 
@@ -74,14 +75,19 @@ function CommitGraphBox(props) {
     const ye = new Intl.DateTimeFormat("en", { year: "numeric" }).format(d);
     const mo = new Intl.DateTimeFormat("en", { month: "2-digit" }).format(d);
     const da = new Intl.DateTimeFormat("en", { day: "2-digit" }).format(d);
-    return `${ye}/${mo}/${da}`;
+    return `${ye}-${mo}-${da}`;
   }
 
   return (
     <div className="project-graph">
-      <CommitGraph graphPoints={graphPoints} />
+      <CommitGraph
+        graphPoints={graphPoints}
+        setTotalCommits={setTotalCommits}
+      />
       <div className="project-graph-label">
         Commits from {printDate(endDates[0])} to {printDate(endDates[1])}
+        <br />
+        Total commits: {totalCommits}
       </div>
     </div>
   );
