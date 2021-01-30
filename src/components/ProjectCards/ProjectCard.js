6172a8561543c7cc6cb62b8428cf7be42b83838b
= <benleong0@gmail.com>
2021-01-30 13:22:18 +0000

    fade in animation on scroll

--- src/components/ProjectCards/ProjectCard.js
+++ src/components/ProjectCards/ProjectCard.js
@@ -2,6 +2,7 @@ import React from "react";
 import CommitGraphBox from "./CommitGraphBox";
 import LinkBox from "./LinkBox";
 import ProjectImage from "./ProjectImage";
+import { Fade } from "react-awesome-reveal";
 
 // <img
 //   src={`${process.env.PUBLIC_URL}/ProjectImages/${project.id}.png`}
@@ -12,34 +13,40 @@ class ProjectCard extends React.Component {
   render() {
     const project = this.props;
     return (
-      <div className="project-card">
-        <div className="project-text col-9">
-          <div className="project-title">{project.name}</div>
-          <div className="project-description">
-            {project.description.map((paragraph, index) => {
-              return <p key={"para" + index}>{paragraph}</p>;
-            })}
+      <Fade
+        className="project-card card-general"
+        direction="up"
+        triggerOnce="true"
+      >
+        <>
+          <div className="project-text col-9">
+            <div className="project-title">{project.name}</div>
+            <div className="project-description">
+              {project.description.map((paragraph, index) => {
+                return <p key={"para" + index}>{paragraph}</p>;
+              })}
+            </div>
           </div>
-        </div>
 
-        <div className="col-3 project-r-col">
-          {project.github_public ? (
-            <CommitGraphBox {...project} />
-          ) : (
-            <ProjectImage {...project} />
-          )}
-        </div>
+          <div className="col-3 project-r-col">
+            {project.github_public ? (
+              <CommitGraphBox {...project} />
+            ) : (
+              <ProjectImage {...project} />
+            )}
+          </div>
 
-        <LinkBox {...project} />
-        <div className="project-tags col-12">
-          <span>Tags: </span>
-          {project.tags.map((tag, index) => (
-            <span key={tag} className="project-tag">
-              {(index > 0 ? " ; " : "") + tag}
-            </span>
-          ))}
-        </div>
-      </div>
+          <LinkBox {...project} />
+          <div className="project-tags col-12">
+            <span>Tags: </span>
+            {project.tags.map((tag, index) => (
+              <span key={tag} className="project-tag">
+                {(index > 0 ? " ; " : "") + tag}
+              </span>
+            ))}
+          </div>
+        </>
+      </Fade>
     );
   }
 }
